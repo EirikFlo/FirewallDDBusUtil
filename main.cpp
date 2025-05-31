4cc7dd6e4e73a7e400674e9f624b0a7ecb77bebd
opply <opply@localhost.localdomain>
2025-05-31 03:48:30 -0400

    Added more test to main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 #include "network-utils/firewalld/qtfirewalldmanager.h" // Updated include path
 #include "network-utils/types/dbus_types.h" // For D-Bus type registration
+#include "network-utils/networkmanager/QtNetworkManager.h"
 
 int main(int argc, char *argv[])
 {
@@ -11,8 +12,14 @@ int main(int argc, char *argv[])
 
     QtFirewalldManager man;
     const auto zones = man.zoneNames();
-
     qDebug() << zones;
+
+    QtNetworkManager nMan;
+    const auto interfaces = nMan.listInterfaceNames();
+    qDebug() << interfaces;
+
+    auto test = nMan.getInterfaceDetails( "ens160" );
+    qDebug() << test.currentIpAddress.toString() << "/" << test.currentPrefix.toString();
 
     return a.exec();
 }
